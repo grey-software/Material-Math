3a157c2d29a25b53150d87e7f571ef62a40024c0
ArsalaBangash <arsalabangash@gmail.com>
2020-08-10 05:18:44 -0400

    Added paypal donate button

--- packages/app/src/pages/HomePage.vue
+++ packages/app/src/pages/HomePage.vue
@@ -2,14 +2,23 @@
   <q-page class="mobile-container">
     <div class="config-header" />
     <div class="column full-viewport-height q-pb-lg">
-      <span class="config-header-label">Hello there!</span>
+      <div class="row config-header-label">
+        <span class="q-mr-lg">Hello there!</span>
+        <a target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VEAGAZP7DHJNE&source=url"><button class="paypal-button" ><img
+            class="paypal-icon"
+            src="https://assets.codepen.io/853141/paypal.png"
+          />Donate</button></a>
+      </div>
       <div class="config-customize-practice row justify-center">
         <customize-practice-card />
       </div>
       <concept-picker />
       <q-space />
       <div class="row justify-center">
-        <router-link style="q-mt-lg" to="/practice">
+        <router-link
+          style="q-mt-lg"
+          to="/practice"
+        >
           <action-button label="Play" />
         </router-link>
       </div>
@@ -27,8 +36,8 @@ export default {
   components: {
     ConceptPicker,
     ActionButton,
-    CustomizePracticeCard
-  }
+    CustomizePracticeCard,
+  },
 };
 </script>
 
@@ -73,5 +82,35 @@ export default {
     flex-direction: column;
     justify-content: space-between;
   }
+}
+.paypal-icon {
+  height: 28px;
+  margin-right: 8px;
+}
+
+.paypal-button {
+  border-radius: 24px;
+  height: 48px;
+  border: 1px solid #003084;
+  outline: none;
+  display: flex;
+  align-items: center;
+  padding: 2px 20px;
+  color: #003084;
+  font-size: 20px;
+  background-color: white;
+  transition: all 0.3s ease-out;
+}
+
+.paypal-button:hover {
+  cursor: pointer;
+  border: 1px solid #1ba0de;
+}
+
+.paypal-button:active {
+  cursor: pointer;
+  border: 1px solid #1ba0de;
+  color: white;
+  background-color: #003084;
 }
 </style>
